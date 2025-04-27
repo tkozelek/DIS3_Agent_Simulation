7df4f68a53bdcf5b917c2e60e8f358cb701a3f28
t.kozelek152 <t.kozelek152@gmail.com>
2025-04-27 14:05:22 +0200

    agent body in central class, composition

---
+++ src/main/java/config/Group.java
@@ -0,0 +1,70 @@
+package config;
+
+import OSPDataStruct.SimQueue;
+import entity.product.Product;
+import entity.worker.Worker;
+import entity.worker.WorkerGroup;
+import entity.worker.WorkerWork;
+
+import java.util.ArrayList;
+
+public class Group {
+    private final SimQueue<Product> queue;
+    private Worker[] workers;
+    private final WorkerGroup workerGroup;
+    private final int amount;
+
+    public Group(int workerAmount, WorkerGroup workerGroup) {
+        this.queue = new SimQueue<>();
+        this.workers = new Worker[workerAmount];
+        this.workerGroup = workerGroup;
+        this.amount = workerAmount;
+    }
+
+    public void addQueue(Product p) {
+        queue.add(p);
+    }
+
+    public Product pollQueue() {
+        return queue.poll();
+    }
+
+    public int queueSize() {
+        return queue.size();
+    }
+
+    public Worker[] getWorkers() {
+        return workers;
+    }
+
+    private void createWorkers() {
+        workers = new Worker[amount];
+        for (int i = 0; i < amount; i++) {
+            workers[i] = new Worker(workerGroup);
+        }
+    }
+
+    public ArrayList<Worker> getFreeWorkers() {
+        ArrayList<Worker> freeWorkers = new ArrayList<>();
+        for (Worker w : workers) {
+            if (w.getCurrentWork() == WorkerWork.IDLE) {
+                freeWorkers.add(w);
+            }
+        }
+        return freeWorkers;
+    }
+
+    public Worker getFreeWorker() {
+        for (Worker worker : workers) {
+            if (worker.getCurrentWork() == WorkerWork.IDLE) {
+                return worker;
+            }
+        }
+        return null;
+    }
+
+    public void reset() {
+        this.queue.clear();
+        this.createWorkers();
+    }
+}
